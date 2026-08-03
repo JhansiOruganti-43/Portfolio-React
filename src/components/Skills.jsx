ebe316b5c1746b1833f754c22d8b2fe7f98a7c85
Jhansi Oruganti <jhansioruganti43@gmail.com>
2026-08-03 23:22:16 +0530

    Start removing client branding from homepage

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -53,6 +53,7 @@ export default function Skills() {
     }
   };
 
+  
   const itemVariants = {
     hidden: { opacity: 0, y: 15 },
     show: { opacity: 1, y: 0, transition: { type: 'spring', stiffness: 120 } }
